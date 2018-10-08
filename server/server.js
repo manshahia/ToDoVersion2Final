f292d02d0ea4fc1cb17b33b04990130b3e9c1c25
ravi <rmanshahia@gmail.com>
2018-10-08 18:29:45 +0530

    Added /GET:id api

--- server/server.js
+++ server/server.js
@@ -4,11 +4,15 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {ToDo} = require('./models/todo');
+var {ObjectID} = require('mongodb');
 
 
 var app = express();
 
 app.use(bodyParser.json());
+
+
+
 
 app.post('/todos', (req,res) => {
   var todo = new ToDo({
@@ -25,11 +29,23 @@ app.get('/todos', (req,res) => {
   }, (e)=> { res.status(400).send(e); });
 });
 
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id))
+    return res.status(404).send({ 'error' : 'Invalid ID'});
+
+    ToDo.findById(id).then( (doc)=> {
+      if(!doc) return res.status(404).send();
+
+      res.send({doc});
+}).catch( (e) => {
+  res.status(400).send();
+});
+});
 
 app.listen(3000, () => {
   console.log("Server Started on port 3000");
-})
-
+});
 //server.js Only responsible for routes
 
 module.exports = {app};
