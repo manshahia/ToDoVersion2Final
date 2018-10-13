979ad380d35428aacf0c05d531d56b016dde90ca
ravi <rmanshahia@gmail.com>
2018-10-13 09:03:07 +0530

    Added Private Route and Auth Middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {ToDo} = require('./models/todo');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -102,6 +103,13 @@ app.post('/users', (req,res) => {
   }).catch((e)=>{
     res.status(400).send(e);
   });
+});
+
+
+
+
+app.post('/users/me', authenticate, (req,res) =>{
+  res.send(req.user);
 });
 
 app.listen(port, () => {
